5ef13e0a57fe92b402726a2428d8c2d290be188b
Hans Peterson <hanspeterson33@gmail.com>
2018-06-30 15:25:29 -0600

    changes

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function profile(req, res) {
 	
 	get_user(id, function(error, result) {
 		if (error || result == null || result.length != 1) {
-			response.status(500).json({success: false, data: error});
+			res.status(500).json({success: false, data: error});
 		} else {
 			var person = result[0];
-			response.status(200).json(result[0]);
+			res.status(200).json(result[0]);
 		}
 	});
 	
