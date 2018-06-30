077975a83b7809e5fe925558b81f65c9b6264d52
Hans Peterson <hanspeterson33@gmail.com>
2018-06-30 13:52:34 -0600

    changes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function homepage (req, req) {
+	res.render('pages/index')
+}
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
@@ -6,5 +10,5 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', homepage)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
