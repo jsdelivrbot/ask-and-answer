cf6e6336d662711a01fde7ec4dc26ff3cf955833
Hans Peterson <hanspeterson33@gmail.com>
2018-07-04 21:25:01 -0600

    stuff

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function browse(req, res) {
 			res.status(500).json({success: false, data: error});
 		} 
 		else {
-			res.status(200).json(result[0]);
+			res.status(200).json(result.rows[0]);
 		}
 	});
 }
