7241409e4d6696edbedadd6bde77e683d722b566
Hans Peterson <hanspeterson33@gmail.com>
2018-07-07 14:27:17 -0600

    stuff

---
+++ ajaxRequest.js
@@ -0,0 +1,12 @@
+function ajaxRequest(url) {
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	
+	xhttp.open("GET", "url", true);
+	xhttp.send();
+}
