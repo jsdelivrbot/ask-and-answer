a118ad208cbdde2b8418299d6d325cfbd1d50e48
Hans Peterson <hanspeterson33@gmail.com>
2018-07-07 19:57:33 -0600

    stuff

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function getQuestion(req, res) {
 	var id = req.query.id;
 	var categoryId = req.query.categoryId;
 	
-	var sql = 'SELECT id, title, content, "date", category_id FROM question WHERE $1::int IS NULL OR id = $1::int AND $2::int IS NULL OR id = $2::int';
+	var sql = 'SELECT id, title, content, "date", category_id FROM question WHERE ($1::int IS NULL OR id = $1::int) AND ($2::int IS NULL OR category_id = $2::int)';
 	var params = [id, categoryId];
 	
 	pool.query(sql, params, function(err, result) {
