2cea065525866443e35e55470018dfa2477d5f18
Hans Peterson <hanspeterson33@gmail.com>
2018-07-07 20:15:20 -0600

    stuff

--- public/javascript/categoriesRequest.js
+++ public/javascript/categoriesRequest.js
@@ -10,7 +10,7 @@ function updateResultList(data) {
 	if (data && data.length > 0) {
 		var categories = $("#selectCategories");
 		categories.empty();
-		categories.append("<option value='none'>None</option>");
+		categories.append("<option value='NULL'>None</option>");
 		
 		for (var i = 0; i < data.length; i++) {
 			var id = data[i].id;
