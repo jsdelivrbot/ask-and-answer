63c14db7f1b46f9a3cb15361d0dc86897c847401
Hans Peterson <hanspeterson33@gmail.com>
2018-07-12 15:35:34 -0600

    stuff

--- public/javascript/question.js
+++ public/javascript/question.js
@@ -31,3 +31,35 @@ function displayQuestion(id) {
 		}
 	});
 }
+
+function displayAnswers(questionID) {
+	var params = {questionID: questionID};
+	
+	$.get("https://polar-everglades-23609.herokuapp.com/getAnswer", params, function(data, status){
+		if (data) {
+			var div = $("#answers");
+			div.empty();
+			
+			for (int i = 0; i < data.length(); i++) {
+				var content = data[i].content;
+				var user_id = data[i].user_id;
+			
+				var params = {id: user_id};
+	
+				$.get("https://polar-everglades-23609.herokuapp.com/getUser", params, function(data, status){
+					if (data) {
+						div.append("<a href='/profile?id=" + data.id + "'>" + data.username + "</a>");
+					}
+					else {
+						console.log("Invalid request!");
+					}
+				});
+			
+				div.append("<p>" + content + "</p>");
+			}
+		}
+		else {
+			console.log("Invalid request!");
+		}
+	});
+}
