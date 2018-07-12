5b5aaef35319683d35f58f5b2a5c5d974098a9f7
Hans Peterson <hanspeterson33@gmail.com>
2018-07-12 16:02:42 -0600

    stuff

--- public/javascript/question.js
+++ public/javascript/question.js
@@ -40,15 +40,15 @@ function displayAnswers(questionID) {
 			var div = $("#answers");
 			div.empty();
 			
-			for (var i = 0; i < data.length; i++) {
-				var content = data[i].content;
-				var user_id = data[i].user_id;
+			for (var obj in data) {
+				var content = obj.content;
+				var user_id = obj.user_id;
 				
 				var params = {id: user_id};
 	
-				$.get("https://polar-everglades-23609.herokuapp.com/getUser", params, function(data, status){
-					if (data) {
-						var username = data.username;
+				$.get("https://polar-everglades-23609.herokuapp.com/getUser", params, function(data2, status2){
+					if (data2) {
+						var username = data2.username;
 						div.append("<div><a href='/profile?id=" + user_id + "'>" + username + "</a><p>" + content + "</p></div>");
 						//div.append("<a href='/profile?id=" + data.id + "'>" + data.username + "</a>");
 					}
